55e08780c2418ff09b130cc9811866d9fdf2ffa1
Vijaya Venkatachalam <vijaya.venkatachalam@gmail.com>
2026-05-26 15:34:01 +0100

    Selenium test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
-assert "Congratulations!" in browser.title
-print("My first test was successful!")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_start_todo_list(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To Do", self.browser.title)
+        self.fail("Finish the test")
+
+if __name__ == "__main__":
+    unittest.main()
